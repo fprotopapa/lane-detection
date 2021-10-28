a584032acd29f4bcd6cf8024d5fd213d94cce195
Fabbio Protopapa <fprotopapa.git@gmail.com>
2021-10-28 21:00:01 +0200

    Add function to retrieve video properties

--- lane_detection.py
+++ lane_detection.py
@@ -18,7 +18,8 @@ import glob
 # User defined configurations
 video_folder_name = "input_video"
 video_file_format = "*.mp4"
-#
+
+# Print System and Software version
 def printVerionInformation():
   print("""
     Python version: {}
@@ -34,6 +35,25 @@ def printVerionInformation():
   platform.platform()
   ))
 
+# Print Video Information, return video resolution (width, height)
+def getVideoInformation(cap, filename):
+  width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+  height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+  print("""
+    File:     {}
+    FPS:      {}
+    # Frames: {}
+    Width:    {}
+    Height:   {}
+    """.format(
+  filename,
+  cap.get(cv2.CAP_PROP_FPS),
+  int(cap.get(cv2.CAP_PROP_FRAME_COUNT)),
+  width,
+  height
+  ))
+  return (width, height)
+
 # Main application
 def main():
   printVerionInformation()
@@ -41,8 +61,7 @@ def main():
   current_dir = os.getcwd()
   vid_path = os.path.join(current_dir, video_folder_name)
   # Search for avaible video files
-  vid_files = glob.glob(os.path.join(vid_path, video_file_format))
-  vid_files.sort()
+  vid_files = sorted(glob.glob(os.path.join(vid_path, video_file_format)))
   end_application = False
   # Loop through found video files
   for vid in vid_files:
@@ -51,6 +70,7 @@ def main():
     if (cap.isOpened()== False):
       print("Error opening file {}".format(vid))
     _, filename = os.path.split(vid)
+    width, height = getVideoInformation(cap, filename)
     # Enter video processing
     while(cap.isOpened()):
       # Read next frame
